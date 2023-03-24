885e0249fd839caf0bb64d5f470c8113f7acb613
Jason Humphrey <jason@codingcareerfastlane.com>
2023-03-23 19:53:33 -0500

    updated script

--- README.md
+++ README.md
@@ -5,6 +5,7 @@
 ``` bash
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
 export NVM_DIR='$HOME/.nvm'
+NVM_DIR='$HOME/.nvm'
 source ~/.bashrc
 nvm install v18.15.0
 git clone https://github.com/VastAiGang/vastaigang-machine-exec.git
--- install.sh
+++ install.sh
@@ -3,7 +3,8 @@
 curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.3/install.sh | bash
 NVM_DIR_VAR='$HOME/.nvm'
 export NVM_DIR=$NVM_DIR_VAR
-echo "export NVM_DIR=$NVM_DIR_VAR"
+NVM_DIR=NVM_DIR_VAR
+echo "exported NVM_DIR=$NVM_DIR_VAR"
 source ~/.bashrc
 NODE_VERSION='v18.15.0'
 echo "Installing node version $NODE_VERSION"
